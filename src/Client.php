84c6a8fc04ccb26aa9ac0b191e8ed2b7b5ba20d7
bogdan <mynameisbogdan@yahoo.com>
2019-09-22 22:27:46 +0300

    bump version

--- src/Client.php
+++ src/Client.php
@@ -17,7 +17,7 @@ use const E_USER_DEPRECATED;
 class Client
 {
     /** Library version */
-    public const VERSION = '0.9.8';
+    public const VERSION = '0.9.9';
 
     /** Default API Uri */
     public const API_URI = 'https://urgentcargus.azure-api.net/api/';
